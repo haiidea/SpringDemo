a2cfdf577a190091aeee3fa4b5092a5e975c090c
haiidea <18101350593@189.cn>
2018-10-17 00:20:08 +0800

    first

---
+++ springboot/src/main/java/com/haiidea/MainApp.java
@@ -0,0 +1,13 @@
+package com.haiidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+    private static final String TAG = "MainApp";
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class,args);
+    }
+}
